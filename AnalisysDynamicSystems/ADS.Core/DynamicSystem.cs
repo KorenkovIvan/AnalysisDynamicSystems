5a009bb005dfcfefe79a2027a92455668f6e40fb
Korenkov-Ivan <KorenkovIM@gmail.com>
2023-07-08 23:41:19 +0300

    Add GetNextVector

--- AnalisysDynamicSystems/ADS.Core/DynamicSystem.cs
+++ AnalisysDynamicSystems/ADS.Core/DynamicSystem.cs
@@ -8,7 +8,8 @@ namespace ADS.Core;
 /// </summary>
 public abstract class DynamicSystem
 {
-    private const string MESSAGE_TRY_GET_NOT_HAVE_PARAMETR = "Нет параметра {0} в динамической системы {1}";
+    private const string
+        MESSAGE_TRY_GET_NOT_HAVE_PARAMETR = "Нет параметра {0} в динамической системы {1}";
     /// <summary>
     /// Наименование Динамической системы
     /// </summary>
@@ -63,4 +64,39 @@ public abstract class DynamicSystem
     /// <param name="vector">Вектор</param>
     /// <returns>Значение производной в векторе </returns>
     public abstract float Fz(Vector3 vector);
+
+    public Vector3 GetNextVector(Vector3 vector, float steap)
+    {
+        if (
+            float.IsNaN(steap) ||
+            float.IsInfinity(steap) ||
+            steap >= 10 ||
+            steap < 0.00000001f)
+            throw new ArgumentException(nameof(steap));
+
+        float[,] k = new float[4, 3];
+
+        k[0, 0] = Fx(vector);
+        k[0, 1] = Fy(vector);
+        k[0, 2] = Fz(vector);
+
+        k[1, 0] = Fx(new Vector3(vector.X + steap / 2, vector.Y + steap / 2 * k[0, 1], vector.Z + steap / 2 * k[0, 2]));
+        k[1, 1] = Fy(new Vector3(vector.X + steap / 2 * k[0, 0], vector.Y + steap / 2, vector.Z + steap / 2 * k[0, 2]));
+        k[1, 2] = Fz(new Vector3(vector.X + steap / 2 * k[0, 0], vector.Y + steap / 2 * k[0, 1], vector.Z + steap / 2));
+
+        k[2, 0] = Fx(new Vector3(vector.X + steap / 2, vector.Y + steap / 2 * k[1, 1], vector.Z + steap / 2 * k[1, 2]));
+        k[2, 1] = Fy(new Vector3(vector.X + steap / 2 * k[1, 0], vector.Y + steap / 2, vector.Z + steap / 2 * k[1, 2]));
+        k[2, 2] = Fz(new Vector3(vector.X + steap / 2 * k[1, 0], vector.Y + steap / 2 * k[1, 1], vector.Z + steap / 2));
+
+        k[3, 0] = Fx(new Vector3(vector.X + steap, vector.Y + steap * k[2, 1], vector.Z + steap * k[2, 2]));
+        k[3, 1] = Fy(new Vector3(vector.X + steap * k[2, 0], vector.Y + steap, vector.Z + steap * k[2, 2]));
+        k[3, 2] = Fz(new Vector3(vector.X + steap * k[2, 0], vector.Y + steap * k[2, 1], vector.Z + steap));
+
+        vector.X += steap / 6 * (k[0, 0] + 2 * k[1, 0] + 2 * k[2, 0] + k[3, 0]);
+        vector.Y += steap / 6 * (k[0, 1] + 2 * k[1, 1] + 2 * k[2, 1] + k[3, 1]);
+        vector.Z += steap / 6 * (k[0, 2] + 2 * k[1, 2] + 2 * k[2, 2] + k[3, 2]);
+
+        return vector;
+
+    }
 }
