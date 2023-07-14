33b6890b3eb5addcb95bb8e5a57fe013fb0c9e66
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 15:10:40 +0300

    fix console

---
+++ AnalisysDynamicSystems/StartConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ADS.Core;
+using ADS.Core.ConcritCalculate;
+using ADS.Core.ConcritDynamicSystems;
+using ADS.Core.ConcritMaping;
+
+var dynamicSystem = new LorenzDynamicSystem();
+var parametrs = new AttractorParametr();
+var attractorCalculate = new AttractorCalculate(dynamicSystem);
+var result = attractorCalculate.GetResult(parametrs);
+var mapintAttractor = new AttractorMaping();
+var matrix = mapintAttractor.GetResult(result, new MapingParametr());
+new CreaterImg().Create(matrix);
