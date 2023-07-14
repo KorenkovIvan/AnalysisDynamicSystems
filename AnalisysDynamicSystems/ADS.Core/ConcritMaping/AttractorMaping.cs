efb82f247bcf87e5685d499e1bad987f85e85494
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 14:58:59 +0300

    fix

---
+++ AnalisysDynamicSystems/ADS.Core/ConcritMaping/AttractorMaping.cs
@@ -0,0 +1,21 @@
+﻿using ADS.Core.ConcritCalculate;
+
+namespace ADS.Core.ConcritMaping;
+
+public class AttractorMaping: Maping<AttractorResult>
+{
+    public override Color[,] GetResult(AttractorResult attractorResult, MapingParametr parametrs)
+    {
+        Color[,] result = new Color[parametrs.Width, parametrs.Height];
+
+        for (int i = 0; i < parametrs.Width; i++)
+        {
+            for (int j = 0; j < parametrs.Height; j++)
+            {
+                result[i, j] = (i + j) % 2 == 0 ? Color.White : Color.Aqua;
+            }
+        }
+        
+        return result;
+    }
+}
--- AnalisysDynamicSystems/ADS.Core/Maping.cs
+++ AnalisysDynamicSystems/ADS.Core/Maping.cs
@@ -2,5 +2,11 @@
 
 public abstract class Maping<TResult>
 {
-    public abstract Color[,] GetResult(TResult result);
+    public abstract Color[,] GetResult(TResult attractorResult, MapingParametr parametrs);
+}
+
+public class MapingParametr
+{
+    public uint Width { get; set; } = 400;
+    public uint Height { get; set; } = 400;
 }
