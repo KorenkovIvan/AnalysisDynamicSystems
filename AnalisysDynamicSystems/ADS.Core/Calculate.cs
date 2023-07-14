faa142a763e493fdc2e5397288b3deeb6b872d16
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 12:57:15 +0300

    Create Calculate.cs
    Add Calculate

---
+++ AnalisysDynamicSystems/ADS.Core/Calculate.cs
@@ -0,0 +1,9 @@
+﻿namespace ADS.Core;
+
+/// <summary>
+/// Класс для вычислений
+/// </summary>
+public abstract class Calculate<TParametr, TResult>
+{
+    public abstract TResult GetResult(TParametr parametr);
+}
