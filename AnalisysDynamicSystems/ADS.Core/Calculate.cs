afb917f3cfaa70cc5df6f52757a6baaa6f6e6305
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 13:45:33 +0300

    fix

--- AnalisysDynamicSystems/ADS.Core/Calculate.cs
+++ AnalisysDynamicSystems/ADS.Core/Calculate.cs
@@ -5,5 +5,11 @@
 /// </summary>
 public abstract class Calculate<TParametr, TResult>
 {
+    public readonly DynamicSystem CurrentDynamicSystem;
     public abstract TResult GetResult(TParametr parametr);
+
+    public Calculate(DynamicSystem dynamicSystem)
+    {
+        CurrentDynamicSystem = dynamicSystem;
+    }
 }
---
+++ AnalisysDynamicSystems/ADS.Core/ConcritCalculate/AttractorCalculate.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace ADS.Core.ConcritCalculate;
+
+public class AttractorCalculate: Calculate<AttractorParametr, Vector3[]>
+{
+    public 
+    
+    public override Vector3[] GetResult(AttractorParametr parametr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AttractorCalculate(DynamicSystem dynamicSystem) 
+        : base(dynamicSystem)
+    {
+    }
+}
+
+public class AttractorParametr
+{
+    public Vector3? StartVector { get; set; } = null;
+    public uint CountSkipPoints { get; set; }
+    public uint CountIteration { get; set; }
+    public float? Steap { get; set; } = null;
+    public bool WithBorders { get; set; } = true;
+}
