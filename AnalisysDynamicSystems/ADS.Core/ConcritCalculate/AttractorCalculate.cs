862d41a5a8187f698f9817525f11a6df9f1e719b
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 14:16:04 +0300

    fix

--- AnalisysDynamicSystems/ADS.Core/ConcritCalculate/AttractorCalculate.cs
+++ AnalisysDynamicSystems/ADS.Core/ConcritCalculate/AttractorCalculate.cs
@@ -2,13 +2,28 @@
 
 namespace ADS.Core.ConcritCalculate;
 
-public class AttractorCalculate: Calculate<AttractorParametr, Vector3[]>
+public class AttractorCalculate: Calculate<AttractorParametr, AttractorResult>
 {
-    public 
-    
-    public override Vector3[] GetResult(AttractorParametr parametr)
+
+    public override AttractorResult GetResult(AttractorParametr parametr)
     {
-        throw new NotImplementedException();
+        Vector3 vector = CurrentDynamicSystem.GetStartVector();
+        AttractorResult result = new()
+        {
+            Trajectory = new Vector3[parametr.CountIteration],
+        };
+        
+        for (int i = 0; i < parametr.CountSkipPoints; i++)
+        {
+            vector = CurrentDynamicSystem.GetNextVector(vector, parametr.Steap);
+        }
+
+        for (int i = 0; i < parametr.CountIteration; i++)
+        {
+            result.Trajectory[i] = CurrentDynamicSystem.GetNextVector(vector, parametr.Steap);
+        }
+
+        return result;
     }
 
     public AttractorCalculate(DynamicSystem dynamicSystem) 
@@ -22,6 +37,17 @@ public class AttractorParametr
     public Vector3? StartVector { get; set; } = null;
     public uint CountSkipPoints { get; set; }
     public uint CountIteration { get; set; }
-    public float? Steap { get; set; } = null;
+    public float Steap { get; set; } = 0.001f;
     public bool WithBorders { get; set; } = true;
+}
+
+public class AttractorResult
+{
+    public Vector3[] Trajectory { get; set; }
+    public float MaxX { get; set; } = float.MinValue;
+    public float MaxY { get; set; } = float.MinValue;
+    public float MaxZ { get; set; } = float.MinValue;
+    public float MinX { get; set; } = float.MaxValue;
+    public float MinY { get; set; } = float.MaxValue;
+    public float MinZ { get; set; } = float.MaxValue;
 }
