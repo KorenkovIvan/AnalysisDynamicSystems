8a9e23215e24d4cc2bd76211347026e811091298
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 17:00:02 +0300

    fix

---
+++ AnalisysDynamicSystems/ADS.Core/ConcritMaping/NidingMaping.cs
@@ -0,0 +1,19 @@
+﻿namespace ADS.Core.ConcritMaping;
+
+public class NidingMaping: Maping<uint[,]>
+{
+    public override Color[,] GetResult(uint[,] attractorResult, MapingParametr parametrs)
+    {
+        var result = new Color[parametrs.Width, parametrs.Height];
+
+        for (int i = 0; i < parametrs.Width; i++)
+        {
+            for (int j = 0; j < parametrs.Height; j++)
+            {
+                result[i, j] = attractorResult[i, j] % 2 == 0 ? Color.Aqua : Color.Black;
+            }
+        }
+        
+        return result;
+    }
+}
