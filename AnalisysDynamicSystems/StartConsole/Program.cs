e56cc4abda8a13d932d8c532c1132d10d7c539a8
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 17:11:36 +0300

    fix

--- AnalisysDynamicSystems/StartConsole/Program.cs
+++ AnalisysDynamicSystems/StartConsole/Program.cs
@@ -2,23 +2,24 @@
 
 using ADS.Core;
 using ADS.Core.ConcritCalculate;
+using ADS.Core.ConcritCalculate.Niding;
 using ADS.Core.ConcritDynamicSystems;
 using ADS.Core.ConcritMaping;
-
-uint a = 0;
-a <<= 1;
+using ADS.Core.OtherCalculation;
 
 var dynamicSystem = new LorenzDynamicSystem();
-var parametrs = new AttractorParametr()
+var mapParametr = new MapingParametr();
+var parametrs = new NidingParametr()
 {
+    Depth = 2,
     CountIteration = 100_000,
     Steap = 0.001f,
 };
-var attractorCalculate = new AttractorCriticalCalculation(dynamicSystem);
+var attractorCalculate = new NidingCalculation(dynamicSystem);
 
 
 
-var result = attractorCalculate.GetResult(parametrs);
-var mapintAttractor = new AttractorCriticalMaping();
-var matrix = mapintAttractor.GetResult(result, new MapingParametr());
+var result = MapCalculate.GetMap(attractorCalculate, parametrs, mapParametr);
+var mapintAttractor = new NidingMaping();
+var matrix = mapintAttractor.GetResult(result, mapParametr);
 new CreaterImg().Create(matrix);
