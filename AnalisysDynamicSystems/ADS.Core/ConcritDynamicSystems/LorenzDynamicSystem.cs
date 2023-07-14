2d36567f6921dd566cd205d2c7bfe3ef77c2d811
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-14 13:28:51 +0300

    fix

---
+++ AnalisysDynamicSystems/ADS.Core/ConcritDynamicSystems/LorenzDynamicSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+namespace ADS.Core.ConcritDynamicSystems;
+
+public class LorenzDynamicSystem: DynamicSystem
+{
+    private const string NAME_DYNAMIC_SYSTEM 
+        = "Система Лорнеца";
+
+    #region Дефолтное значение параметров
+
+    public static readonly Parametr Sigma = new()
+    {
+        Name = nameof(Sigma),
+        Value = 10f,
+    };
+    public static readonly Parametr R = new()
+    {
+        Name = nameof(R),
+        Value = 28f,
+    };
+    public static readonly Parametr B = new()
+    {
+        Name = nameof(B),
+        Value = 8f/3f,
+    };
+
+    #endregion
+    
+    public LorenzDynamicSystem() 
+        : base(NAME_DYNAMIC_SYSTEM, Sigma, R, B) { }
+
+    public override float Fx(Vector3 vector) => this[nameof(Sigma)] * (vector.Y - vector.X);
+    public override float Fy(Vector3 vector) => vector.X * (this[nameof(R)] - vector.Z) - vector.Y;
+    public override float Fz(Vector3 vector) => vector.X * vector.Y - this[nameof(B)] * vector.Z;
+
+    public override Vector3 GetStartVector() => new(0.01f, 0f, 0f);
+}
