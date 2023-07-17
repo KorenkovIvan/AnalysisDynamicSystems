3fe706385c4a31e572499d2c8d3238dab21f133a
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-17 16:40:53 +0300

    fix

--- AnalisysDynamicSystems/ADS.Core/ConcritMaping/AttractorLyapynovMaping.cs
+++ AnalisysDynamicSystems/ADS.Core/ConcritMaping/AttractorLyapynovMaping.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using ADS.Core.ConcritCalculate.Attractor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ADS.Core.ConcritMaping
 {
-    internal class AttractorLyapynovMaping
+    public class AttractorLyapynovMaping : Maping<LyapynovAttractorResult>
     {
+        public override Color[,] GetResult(LyapynovAttractorResult attractorResult, MapingParametr parametrs)
+        {
+            Color[,] result = new Color[parametrs.Width, parametrs.Height];
+
+            for (int i = 0; i < parametrs.Width; i++)
+            {
+                for (int j = 0; j < parametrs.Height; j++)
+                {
+                    result[i, j] = Color.White;
+                }
+            }
+
+            for (int i = 0; i < attractorResult.Trajectory.Length; i++)
+            {
+                var x = (attractorResult.Trajectory[i].X - attractorResult.MinX) /
+                    (attractorResult.MaxX - attractorResult.MinX) * parametrs.Width;
+                var y = (attractorResult.Trajectory[i].Z - attractorResult.MinZ) /
+                    (attractorResult.MaxZ - attractorResult.MinZ) * parametrs.Height;
+                if (0 <= x && x < parametrs.Width && 0 <= y && y < parametrs.Height)
+                {
+                    var buff = attractorResult.Ecu[i].Length() / attractorResult.Eps > 0;
+
+                    result[(int)x, (int)y] = buff ?
+                        new Color(new Vector4(1f, 0f, 0f, 1f)) :
+                        new Color(new Vector4(0f, 0f, 1f, 1f));
+                }
+            }
+
+            return result;
+        }
     }
 }
--- AnalisysDynamicSystems/StartConsole/Program.cs
+++ AnalisysDynamicSystems/StartConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ADS.Core;
 using ADS.Core.ConcritCalculate;
+using ADS.Core.ConcritCalculate.Attractor;
 using ADS.Core.ConcritCalculate.Niding;
 using ADS.Core.ConcritDynamicSystems;
 using ADS.Core.ConcritMaping;
@@ -7,31 +8,46 @@ using ADS.Core.ConcritMaping.MapColor;
 using ADS.Core.ConsoleWriter;
 using ADS.Core.OtherCalculation;
 
-var dynamicSystem = new ShimizyMoriokaDynamicSystem();
 var mapParametr = new MapingParametr()
 {
-    Width = 100,
-    Height = 100,
-    
+    Width = 800,
+    Height = 800,
+
     NameParametrWidth = nameof(ShimizyMoriokaDynamicSystem.Lambda),
     StartParametrWidth = 0f,
     EndParametrWidth = 2f,
-    
+
     NameParametrHeight = nameof(ShimizyMoriokaDynamicSystem.Alpha),
     StartParametrHeight = 0f,
     EndParametrHeight = 2f,
-};
-var parametrs = new NidingParametr()
-{
-    Depth = 12,
-    CountIteration = 1_000_000,
-    Steap = 0.01f,
-};
-var attractorCalculate = new NidingCalculation(dynamicSystem);
 
-var _consoleWriter = new DefaultConsoleWriter();
-ParallelMapCalculate.OnCloseRow += _consoleWriter.Write;
-var result = ParallelMapCalculate.GetMap(attractorCalculate, parametrs, mapParametr);
-var mapintAttractor = new NidingMaping(new DefaultNidingMapColor());
-var matrix = mapintAttractor.GetResult(result, mapParametr);
+    
+};
+
+var dynamicSystem = new LorenzDynamicSystem();
+var calculate = new LyapynovAttractorCalculate(dynamicSystem);
+var maper = new AttractorLyapynovMaping();
+var result = calculate.GetResult(new AttractorParametr()
+{
+    CountIteration = 1_000_000,
+    Steap = 0.001f,
+});
+var matrix = maper.GetResult(result, mapParametr);
 new CreaterImg().Create(matrix);
+
+//var dynamicSystem = new ShimizyMoriokaDynamicSystem();
+
+//var parametrs = new NidingParametr()
+//{
+//    Depth = 12,
+//    CountIteration = 1_000_000,
+//    Steap = 0.01f,
+//};
+//var attractorCalculate = new NidingCalculation(dynamicSystem);
+
+//var _consoleWriter = new DefaultConsoleWriter();
+//ParallelMapCalculate.OnCloseRow += _consoleWriter.Write;
+//var result = ParallelMapCalculate.GetMap(attractorCalculate, parametrs, mapParametr);
+//var mapintAttractor = new NidingMaping(new DefaultNidingMapColor());
+//var matrix = mapintAttractor.GetResult(result, mapParametr);
+//new CreaterImg().Create(matrix);
