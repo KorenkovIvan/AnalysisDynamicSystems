954df44c1c7107d0945ba5175f87c26dd15c260b
Korenkov Ivan <138324616+KorenkovIvan@users.noreply.github.com>
2023-07-27 16:19:14 +0300

    fix

---
+++ AnalisysDynamicSystems/ADS.WebApp/Controllers/DynamicSystems.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADS.WebApp.Controllers
+{
+    public class DynamicSystems : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
